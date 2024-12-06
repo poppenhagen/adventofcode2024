e790ea0c6c81d4933a77bbd314a3e8591ad72bb9
Adam Poppenhagen <apoppenhagen@blueorigin.com>
2024-12-06 12:25:06 -0700

    Getline parsing and report vector generation. Needs filtering

--- poppenhagen/day2/solution1.cpp
+++ poppenhagen/day2/solution1.cpp
@@ -4,18 +4,46 @@
 #include <cmath>
 #include <fstream>
 #include <sstream>
+#include <string>
 
 using namespace std;
 
+vector<int> split_line(string line){
+
+    vector<int> numbers;
+
+    stringstream ss(line);
+    int num;
+
+    while(ss >> num) {
+        numbers.push_back(num);
+    }
+    // for (int n : numbers){  <------ SAVING BECAUSE THIS LOOP RANGE IS USEFUL
+    //     cout << n << endl;
+    // }
+    return numbers;
+
+};
+
 int main(){
     ifstream inputFile("smallinput");
-    string line;
+    string raw_input;
+    vector<vector <int>> reports; 
 
     if(inputFile.is_open()){
-        while(getline(inputFile, line)){
-        cout << line << endl;
+            while(getline(inputFile, raw_input)){
+                reports.push_back(split_line(raw_input));
+            }
+
         }
-    }
+    
+    // for (int i = 0; i < size(reports); i++){ <------ FOR CHECKING REPORTS
+    //     for (int n : reports[i]){
+    //         cout << n << ", ";
+    //     }
+    //     cout << endl;
+    // }
+    
 
 }
 
